03dd336dc642a341b527883cd57e49106d913647
timo <tpetmanson@gmail.com>
2019-01-15 00:17:22 +0100

    Clean up Matemaatika class and add it to Manifest

--- app/src/main/java/com/bavuwe/matemaatika/Matemaatika.java
+++ app/src/main/java/com/bavuwe/matemaatika/Matemaatika.java
@@ -1,43 +1,33 @@
 package com.bavuwe.matemaatika;
 
 import android.app.Application;
-import android.content.SharedPreferences;
 
+/**
+ * Matemaatika application i.e global state.
+ */
 public class Matemaatika extends Application {
-    static final int LOAD_DIALOG = 0;
-
-    // the titles of the different topics
     public static String[] classTitles = null;
     public static String[] topicTitles = null;
     public static String[] subTopicTitles = null;
 
-    // the HTML content of the subtopics
+    /**
+     * HTML contents of the subtopics, all loaded in memory.
+     */
     public static String[] subTopicContents = null;
 
-    // which topics are in what classes
-    // and what subtopics in what topics
+    /**
+     * Denotes, which topics belong to which classes.
+     */
     public static int[][] classTopics = null;
+
+    /**
+     * Denotes, which subtopics belong to which topics.
+     */
     public static int[][] topicSubTopics = null;
 
-    // the index
+    /**
+     * The index for performing searches.
+     */
     public static ContentIndex index = null;
-
-
-    /**
-     * Should we show the introduction tutorial to the user?
-     */
-    public boolean showTutorial() {
-        SharedPreferences prefs = getSharedPreferences("Matemaatika", 0);
-        return Boolean.parseBoolean(prefs.getString("showTutorial", "true"));
-    }
-
-    /**
-     * Set if we should show the introduction to the user.
-     * @param b
-     */
-    public void setShowTutorial(boolean b) {
-        SharedPreferences prefs = getSharedPreferences("Matemaatika", 0);
-        prefs.edit().putBoolean("showTutorial", true);
-    }
 
 }
