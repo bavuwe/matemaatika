cf5555aa0b772fb6c3b45c695c2f69a1f302444e
timo <tpetmanson@gmail.com>
2019-03-27 00:46:54 +0100

    Removed class suffix from materials, because search already shows class and topic

--- materials/lyx/prepare_materials.sh
+++ materials/lyx/prepare_materials.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 elyxer --noconvert latex_content.lyx mata.html
 java -jar ../MataParser/MataIndex.jar
+
+mv mata.html ../../app/src/main/assets
+mv mata.index ../../app/src/main/assets
+mv mata.structure ../../app/src/main/assets
+
